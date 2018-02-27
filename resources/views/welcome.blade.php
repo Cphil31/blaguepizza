8d63167e8b08a699277ad2f8e8349d046693c1af
Cphil31 <cecilephilippe31@gmail.com>
2018-02-27 13:45:27 +0100

    texte centré

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,17 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    
+<div class="container">
+  <div class="row">
+    <div class="col"></div>
+    <div class="col-3">
+        <h1>Hello, world!</h1>
+    </div>
+    <div class="col"></div>
+  </div>
+
+</div>
 
     
     
