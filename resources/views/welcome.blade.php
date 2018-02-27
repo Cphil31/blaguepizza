93ddb4e08878bedb4c7222f6860abd37a0039a24
Cphil31 <cecilephilippe31@gmail.com>
2018-02-27 14:39:57 +0100

    mise en page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,8 @@
 @section('title', 'Page Title')
 
 @section('content')
-    <div class="container">
-  <div class="row">
-    <div class="col"></div>
-    <div class="col-3">
-        <h1>Blague Pizza</h1>
-    </div>
-    <div class="col"></div>
-  </div>
+  
+<div class="acceuil"></div>
 
-</div>
+ 
 @endsection
