158cbe81042a561e4de06b92729cd8c66b0defc9
Cphil31 <cecilephilippe31@gmail.com>
2018-04-20 18:22:59 +0200

    mise en page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,13 @@
 
 @section('content')
   
-<div class="acceuil">Bienvenue chez Blaguepizza</div>
-<div class="acceuil">Passez votre commande</div>
+<div class="bienvenue">Bienvenue chez Blaguepizza</div>
+<div class="passez">Passez votre commande</div>
+<div class="regardez">Regardez nos 
+<a href="/pizzas" style="color: white;">Pizzas</a>
+ et 
+<a href="/pastas" style="color: white;">Pastas</a>
+</div>
 
 
 
