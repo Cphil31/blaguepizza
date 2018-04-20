023a887d5e4ce7857c71eacd603c6b9372dac206
Cphil31 <cecilephilippe31@gmail.com>
2018-04-20 23:29:12 +0200

    mise en page

--- resources/views/layouts/layoutmaster.blade.php
+++ resources/views/layouts/layoutmaster.blade.php
@@ -19,6 +19,7 @@
 
   <div class="titre">
           <div class="row">
+            <div class="col-"></div>
             <div class="col-"><a href="/" style="color:white;">Blague Pizza</a></div>
             <div class="col"></div>
             <div class="col-2">
@@ -30,6 +31,7 @@
             <div class="col-2">
               <a class="menu" href="/contact" style="color:white;">Contact</a>
             </div> 
+            <div class="col-"></div>
           
       </div>
   </div>
