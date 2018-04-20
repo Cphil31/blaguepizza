573c80c4a6dce5ff34c7c0a987d7f1c24f7bdec7
Cphil31 <cecilephilippe31@gmail.com>
2018-04-20 20:36:14 +0200

    mise en page

--- resources/views/pastas.blade.php
+++ resources/views/pastas.blade.php
@@ -3,7 +3,15 @@
 @section('title', 'pastas')
 
 @section('content')
-  
-<div class="textpastas">Nos Pastas</div>
+<div class="row">
+	<div class="col-2"></div>
+	<div class="col">
+		<div class="fond">
+
+			<div class="textpastas">Nos Pastas</div>
+		</div>
+	</div>
+	<div class="col-2"></div>
+</div>
 
 @endsection
