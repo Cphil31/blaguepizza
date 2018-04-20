f485d30c65cf8ab8fa1161c742d2702f22759103
Cphil31 <cecilephilippe31@gmail.com>
2018-04-20 20:03:59 +0200

    mise en page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,25 @@
 @section('title', 'acceuil')
 
 @section('content')
-  
-<div class="bienvenue">Bienvenue chez Blaguepizza</div>
-<div class="passez">Passez votre commande</div>
-<div class="regardez">Regardez nos 
-<a href="/pizzas" style="color: white;">Pizzas</a>
- et 
-<a href="/pastas" style="color: white;">Pastas</a>
-</div>
+<div class="row">
+<div class="col-2"></div>	
+<div class="col">
+	
+	<div class="fond">
 
+		<div class="bienvenue">Bienvenue chez Blaguepizza</div>
+		<div class="regardez">Regardez nos 
+			<a href="/pizzas" style="color: white;">Pizzas</a>
+			et 
+			<a href="/pastas" style="color: white;">Pastas</a>
+		</div>
+		<div class="passez">Passez votre commande <a href="/contact" style="color: white;">ici</a></div>
+
+	</div>
+</div>
+<div class="col-2"></div>	
+</div>	
+<!-- source : https://pixabay.com/fr/arri%C3%A8re-plan-cuit-fromage-cercle-3007395/ -->
 
 
 @endsection
